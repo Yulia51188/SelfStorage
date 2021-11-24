6cc92ffc25d5543663e27bf444a00ab855ac9133
Yulia51188 <sun51188@gmail.com>
2021-11-24 10:04:26 +0300

    Create greeting bot

---
+++ tg_bot.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+from textwrap import dedent
+from enum import Enum
+import os
+from dotenv import load_dotenv
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class States(Enum):
+    CHOOSE_STORAGE = 0
+
+
+def start(update, context):
+    
+    reply_keyboard = [['Склад 1', 'Склад 2', 'Склад 3']]
+
+    update.message.reply_text(
+        dedent('''\
+        Привет!
+        Я помогу вам арендовать личную ячейку для хранения вещей.
+        Давайте посмотрим адреса складов, чтобы выбрать ближайший!
+        '''),
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+        ),
+    )
+
+    return States.CHOOSE_STORAGE
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def handle_unknown(update, context):
+    update.message.reply_text(
+        text='Извините, но я вас не понял :(',
+    )
+
+
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    load_dotenv()
+    tg_token = os.getenv('TG_TOKEN')
+
+    updater = Updater(tg_token)
+
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            States.CHOOSE_STORAGE: [
+                MessageHandler(Filters.text & ~Filters.command, echo)
+            ],
+        },
+        fallbacks=[
+            MessageHandler(Filters.text & ~Filters.command, handle_unknown)
+        ],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
