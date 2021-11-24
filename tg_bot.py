9bad2a683b8013c217657c37f63081a880d6bc85
Yulia51188 <sun51188@gmail.com>
2021-11-24 11:59:17 +0300

    Move functions with db interactions to db_proccessing.py

--- tg_bot.py
+++ tg_bot.py
@@ -1,22 +1,15 @@
 import logging
-from textwrap import dedent
-from enum import Enum
 import os
-from dotenv import load_dotenv
-from rejson import Client, Path
+from enum import Enum
+from textwrap import dedent
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, KeyboardButton
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    ConversationHandler,
-    # CallbackContext,
-)
+from dotenv import load_dotenv
+from telegram.ext import (CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
+
+import db_processing
 
 logger = logging.getLogger(__name__)
-_database = None
 _booking = None
 
 
@@ -24,93 +17,6 @@ class States(Enum):
     CHOOSE_STORAGE = 1
     CHOOSE_CATEGORY = 2
     CHOOSE_STUFF = 3
-
-
-def get_database_connection():
-    """Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан."""
-    global _database
-    if _database is None:
-        database_password = os.getenv("DB_PASSWORD", default=None)
-        database_host = os.getenv("DB_HOST", default='localhost')
-        database_port = os.getenv("DB_PORT", default=6379)
-        _database = Client(host=database_host, port=database_port,
-            password=database_password, decode_responses=True)
-    return _database
-
-
-def create_stogares_keyboard():
-    db = get_database_connection()
-    storages = db.jsonget('storages', Path.rootPath())
-    keyboard = []
-    for storage in storages:
-        keyboard.append(
-            [
-                KeyboardButton(
-                    text=(f'{storage["storage_id"]}. {storage["name"]}'
-                          f'({storage["address"]})')
-                ),
-            ],
-        )
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-
-def create_categories_keyboard():
-    keyboard = [
-        [KeyboardButton(text='Сезонные вещи')],
-        [KeyboardButton(text='Другое')],
-    ]
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-
-def create_other_keyboard():
-    db = get_database_connection()
-    prices = db.jsonget('prices', Path.rootPath())
-    category_stuffs = prices[0]['other']
-    
-    keyboard = []
-    for stuff in category_stuffs:
-        keyboard.append(
-            [
-                KeyboardButton(
-                    text=(f'{stuff["name"]} - {stuff["base_price"]} руб. '
-                          f'(добавить еще + {stuff["add_one_price"]} руб.)')
-                ),
-            ],
-        )
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-
-def create_season_keyboard():
-    db = get_database_connection()
-    prices = db.jsonget('prices', Path.rootPath())
-    category_stuffs = prices[0]['season']
-    
-    keyboard = []
-    for stuff in category_stuffs:
-        if stuff["price"]["week"]:
-            keyboard.append(
-                [
-                    KeyboardButton(
-                        text=(
-                            f'{stuff["item_id"]}. {stuff["name"]} ( '
-                            f'{stuff["price"]["week"]} руб. в неделю или '
-                            f'{stuff["price"]["month"]} руб. в месяц)'
-                        )
-                    )
-                ],
-            )
-        else:
-            keyboard.append(
-                [
-                    KeyboardButton(
-                        text=(
-                            f'{stuff["item_id"]}. {stuff["name"]} ( '
-                            f'{stuff["price"]["month"]} руб. в месяц)'
-                        )
-                    )
-                ],
-            )  
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 
 def start(update, context):
@@ -121,7 +27,7 @@ def start(update, context):
         Я помогу вам арендовать личную ячейку для хранения вещей.
         Давайте посмотрим адреса складов в Москве, чтобы выбрать ближайший!
         '''),
-        reply_markup=create_stogares_keyboard()
+        reply_markup=db_processing.create_stogares_keyboard()
     )
 
     return States.CHOOSE_STORAGE
@@ -141,7 +47,7 @@ def handle_storage_choice(update, context):
     create_new_booking(update.message)
     update.message.reply_text(
         'Что хотите хранить?',
-        reply_markup=create_categories_keyboard()
+        reply_markup=db_processing.create_categories_keyboard()
     )    
     return States.CHOOSE_CATEGORY
 
@@ -150,7 +56,7 @@ def handle_season_choice(update, context):
     add_category_to_booking('season')
     update.message.reply_text(
         'Выберите, какую вещь будете хранить',
-        reply_markup=create_season_keyboard()
+        reply_markup=db_processing.create_season_keyboard()
     ) 
     return States.CHOOSE_STUFF
 
@@ -159,7 +65,7 @@ def handle_other_choice(update, context):
     add_category_to_booking('other')
     update.message.reply_text(
         'Выберите, какую вещь будете хранить',
-        reply_markup=create_other_keyboard()
+        reply_markup=db_processing.create_other_keyboard()
     )     
     return States.CHOOSE_STUFF
 
@@ -174,8 +80,7 @@ def create_new_booking(tg_message):
         'tg_chat_id': tg_message.chat_id,
     }
 
-    db = get_database_connection()
-    _booking['booking_id'] = db.jsonarrlen('bookings', Path.rootPath())
+    _booking['booking_id'] = db_processing.get_bookings_count()
     logger.info(f'New booking is {_booking}')
 
 
