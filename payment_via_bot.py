5ebae585a0ec00ab6dc84e00d3592b5ed4a3b2f2
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2021-11-25 16:08:46 +0300

    Доделал самостоятельного бота для оплаты

--- payment_via_bot.py
+++ payment_via_bot.py
@@ -1,5 +1,8 @@
 import os
+from pprint import pprint
 from dotenv import load_dotenv
+from rejson import Client, Path
+
 from telegram import LabeledPrice, ShippingOption, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Updater,
@@ -11,6 +14,31 @@ from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler,
 )
+
+
+_booking = {
+        'booking_id': '1',
+        'storage_id': '2',
+        'client_id': '1967131305',
+        'category': 'season',
+        'item_id': '4',
+        'count': 2,
+        'period_type': 'week',
+        'period_lenght': 1,
+        'start_date': '2021-11-25',
+        'end_date': '2021-12-01',
+        'total_cost': 600,
+        'status': 'created',
+    }
+
+
+def get_database_connection():
+    database_password = '70uD9NjLBZhP7MvxKgpydqbPYyqzK4mU'
+    database_host = 'redis-13524.c250.eu-central-1-1.ec2.cloud.redislabs.com'
+    database_port = '13524'
+    database = Client(host=database_host, port=database_port,
+        password=database_password, decode_responses=True)
+    return database
 
 
 def start_callback(update: Update, context: CallbackContext) -> None:
@@ -25,19 +53,19 @@ def start_callback(update: Update, context: CallbackContext) -> None:
 
 def start_without_shipping_callback(update: Update, context: CallbackContext):
     """Sends an invoice without shipping-payment."""
-#     query = update.callback_query
-#     query.answer()
+    global _booking, provider_token
+    db = get_database_connection()
+    booking_id = _booking['booking_id']
     
     chat_id = update.callback_query.message.chat_id
-    title = "Payment Example"
-    description = "Payment Example using python-telegram-bot"
+    title = "Оплата бронирования"
+    description = f"Оплата категории {_booking['category']}"
     # select a payload just for you to recognize its the donation from your bot
     payload = "Fuppergupper"
     # In order to get a provider_token see https://core.telegram.org/bots/payments#getting-a-token
-    provider_token = "381764678:TEST:31186"
     currency = "RUB"
     # price in dollars
-    price = 100
+    price = _booking['total_cost']
     # price * 100 so as to include 2 decimal points
     prices = [LabeledPrice("Test", price * 100)]
 
@@ -61,57 +89,38 @@ def precheckout_callback(update: Update, context: CallbackContext) -> None:
 
 # finally, after contacting the payment provider...
 def successful_payment_callback(update: Update, context: CallbackContext) -> None:
-    """Confirms the successful payment."""
-    # do something after successfully receiving payment?
-    update.message.reply_text("Thank you for your payment!")
+    global _booking
+    db = get_database_connection()
+    booking_id = _booking['booking_id']
 
+    db.jsonset('bookings', Path(f'.{booking_id}.status'), 'payed')
+    booking = db.jsonget('bookings', Path(f'.{booking_id}'))
+    pprint(booking)
 
 def create_button(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1')]]
+    keyboard = [[InlineKeyboardButton("PAY", callback_data='1')]]
     
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text('Give me your money', reply_markup=reply_markup)
 
 
-def button(update: Update, context: CallbackContext) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-    query = update.callback_query
-    query.answer()
-    
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Displays info on how to use the bot."""
-    update.message.reply_text("Use /start to test this bot.")
-
-
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
+    global provider_token
     load_dotenv()
     token = os.environ['TELEGRAM_TOKEN']
-    
-    booking_id = 1
-    
+    provider_token = os.environ['PROVIDER_TOKEN']
     updater = Updater(token)    
     dispatcher = updater.dispatcher
-    updater.dispatcher.add_handler(CommandHandler('start', create_button))
     
-    updater.dispatcher.add_handler(CallbackQueryHandler(start_without_shipping_callback))
-    
-
-    dispatcher.add_handler(CommandHandler("noshipping", start_without_shipping_callback))
-
+    dispatcher.add_handler(CommandHandler('start', create_button))
+    dispatcher.add_handler(CallbackQueryHandler(start_without_shipping_callback))
     # Pre-checkout handler to final check
     dispatcher.add_handler(PreCheckoutQueryHandler(precheckout_callback))
-
-    # Success! Notify your user!
     dispatcher.add_handler(MessageHandler(Filters.successful_payment, successful_payment_callback))
-
-    
-    
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
 
     # Start the Bot
     updater.start_polling()
